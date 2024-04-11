da617139d6a458050767356126921b2707a52999
FernandJerico <fernand.jerico.ti.20@cic.ac.id>
2024-04-11 13:54:04 +0700

    Refactor PatientScheduleRemoteDatasources class and add getPatientSchedules method

--- lib/features/patient-schedule/data/datasources/patient_schedule_remote_datasources.dart
+++ lib/features/patient-schedule/data/datasources/patient_schedule_remote_datasources.dart
@@ -1,1 +1,27 @@
-class PatientScheduleRemoteDatasources {}
+import 'package:clinic_management_app/features/patient-schedule/data/models/response/patient_schedule_response_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../../core/constants/variables.dart';
+import '../../../auth/data/datasources/auth_local_datasources.dart';
+
+class PatientScheduleRemoteDatasources {
+  Future<Either<String, PatientScheduleResponseModel>>
+      getPatientSchedules() async {
+    final authData = await AuthLocalDatasources().getAuthData();
+    final url = Uri.parse('${Variables.baseUrl}/api/api-patient-schedules');
+    final response = await http.post(
+      url,
+      headers: {
+        'Authorization': 'Bearer ${authData?.token}',
+        'Accept': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return Right(PatientScheduleResponseModel.fromJson(response.body));
+    } else {
+      return const Left('Failed to add reservation');
+    }
+  }
+}
